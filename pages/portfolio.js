d5b9119e87b602ceccf9c27d2bee694ae840995b
unknown <gilangcpurna@gmail.com>
2023-06-26 16:17:35 +0700

    first commit

---
+++ pages/portfolio.js
@@ -0,0 +1,13 @@
+"use client"
+import React, { useState } from 'react';
+import '../styles/portfolio.css';
+
+const Portfolio = () => {
+    return(
+        <div className="container">
+            <h1>Portfolio</h1>
+        </div>
+    )
+}
+
+export default Portfolio;
